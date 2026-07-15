91f36a832f35d870a3779268648804122af37bc3
julissao205 <julissaorsborn@gmail.com>
2026-07-14 20:28:48 -0500

    modified server.js

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,33 @@
 const express = require("express");
+const { MongoClient } = require("mongodb");
 const app = express();
+const uri = "mongodb://127.0.0.1:27017";
+const client = new MongoClient(uri);
 
-app.get("/", (req, res) => {
-    res.send("backend is running");
-});
+async function startServer() {
+    try {
+        await client.connect();
+        console.log("Connected to MongoDB");
+        
+        const db = client.db("school_demo");
+        const studentsCollection = db.collection("students");
+        
+        await studentsCollection.insertOne({
+            firstName: "John",
+            lastname: "Doe",
+            gradeLevel: 10
+        });
+        
+        app.get("/", (req, res) => {
+            res.send("Backend connected to MongoDB");
+        });
+        
+        app.listen(3000, () => {
+            console.log("Server running at http://localhost:3000");
+        });
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+    }
+}
 
-app.listen(3000, () => {
-    console.log("server running at http://localhost:3000");
-});
+startServer();
